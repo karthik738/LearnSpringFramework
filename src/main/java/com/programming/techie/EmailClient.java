0b63cacb4fd34d8de61e02ec315906a1ebe8daa4
karthik738 <151852747+karthik738@users.noreply.github.com>
2026-04-15 22:12:16 +0530

    Refactor EmailClient for loose coupling with SpellChecker
    Refactored EmailClient to use dependency injection for SpellChecker.

--- src/main/java/com/programming/techie/EmailClient.java
+++ src/main/java/com/programming/techie/EmailClient.java
@@ -7,12 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 class EmailClient {
 
-    // // manual object creation
+    //  manual object creation
     // private BasicSpellChecker basicSpellChecker; 
-    // // manual object creation
+    // private AdvancedSpellChecker advancedSpellChecker; 
+    //  manual object creation
     // EmailClient(){
-    //     this.basicSpellChecker=new BasicSpellChecker();
+    //     this.basicSpellChecker=new BasicSpellChecker(); // tightly coupled with basic spell checker
+           // this.advancedSpellChecker=new AdvancedSpellChecker();  // tightly coupled with advanced spell checker
     // }
+
+    // loosely coupled  - use interface implementation
+    // private SpellChecker spellChecker; 
+
+    //  EmailClient(SpellChecker spellChecker){
+        // this.spellChecker=spellChecker; // loosely coupled
+    // }
+    
 
     @Autowired
     @Qualifier("advancedSpellChecker")
@@ -20,6 +30,8 @@ class EmailClient {
 
     void sendEmail(String emailMessage) {
         // basicSpellChecker.checkSpelling(emailMessage); // manual object creation 
+        // advancedSpellChecker.checkSpelling(emailMessage); // manual object creation 
+        // spellChecker.checkSpelling(emailMessage); // loosely coupled
         spellChecker.checkSpelling(emailMessage);
         // Logic to Send Email
     }
